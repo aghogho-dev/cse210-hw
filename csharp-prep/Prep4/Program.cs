f2540820b0e7111427825047a90ce4652439ddbb
aghogho <aghoghomonorien@gmail.com>
2024-04-13 02:11:12 +0100

    Prep 4 - Lists of Numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
 
 namespace Prep4
 {
@@ -7,7 +10,33 @@ namespace Prep4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is Prep 4");
+            Console.Write("Enter a list of numbers, type 0 when finished.\n");
+
+            List<int> numList = new List<int>();
+
+            while (true) 
+            {
+                Console.Write("Enter number: ");
+                string num = Console.ReadLine();
+                int numInt = int.Parse(num);
+
+                if (numInt == 0) break;
+
+                numList.Add(numInt);
+
+            }
+
+            Console.WriteLine($"\nThe sum is: {numList.Sum()}");
+            Console.WriteLine($"The average is: {numList.Average()}");
+            Console.WriteLine($"The largest number is: {numList.Max()}");
+            Console.WriteLine($"The smallest positive number is: {numList.Where(n => n > 0).Min()}");
+
+            numList.Sort();
+
+            Console.WriteLine("\nThe sorted list is:");
+
+            foreach (int number in numList) Console.WriteLine(number);
+        
         }
     }
 }
